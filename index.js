230a069b3ede6804275bf4b6f12dccd631ea0318
Drew Pereli <drewpereli@gmail.com>
2021-07-31 19:18:46 -0700

    Set up very basic page

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import "./src/styles/app.css";
+
+document.querySelectorAll(".main-nav-item").forEach(el => {
+  let sectionName = el.dataset.section;
+
+  el.addEventListener("click", () => {
+    let section = document.getElementById(`section-${sectionName}`);
+    section?.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+document.getElementById("contact-form")?.addEventListener("submit", e => {
+  e.preventDefault();
+  // let form = e.target;
+  console.log("submitting the form");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "header-piano": "url('../images/header-piano.jpg')",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
