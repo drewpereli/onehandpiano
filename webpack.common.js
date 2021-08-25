3b0475a17b694fffa2b55887601e08a7c6259081
Drew Pereli <drewpereli@gmail.com>
2021-08-24 22:25:35 -0700

    Reconfigure webpack prod config to allow deployment with github actions

--- webpack.common.js
+++ webpack.common.js
@@ -3,8 +3,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const purgeCSS = require("@fullhuman/postcss-purgecss");
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./index.js",
@@ -12,15 +10,9 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     filename: "index_bundle.js",
   },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-  },
   plugins: [
     new HtmlWebpackPlugin({ template: "./src/index.pug" }),
     new MiniCssExtractPlugin(),
-    new FaviconsWebpackPlugin("./src/images/favicon.png"),
-    new Dotenv(),
   ],
   module: {
     rules: [
@@ -44,7 +36,7 @@ module.exports = {
                     content: ["./**/*.pug", "./**/*.html"],
                     extractors: [
                       {
-                        extractor: content => {
+                        extractor: (content) => {
                           return content.match(/[A-z0-9-:\/]+/g) || [];
                         },
                         extensions: ["css", "html", "pug"],
